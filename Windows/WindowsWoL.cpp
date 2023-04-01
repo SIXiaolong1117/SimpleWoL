a9d356270107c74a7415947b31bf710b76a8fd51
SI_Xiaolong <sixiaolong2018@outlook.com>
2023-04-01 15:30:40 +0800

    Windows版本 去掉错误解耦的 发送Magic Packet 函数

--- Windows/WindowsWoL.cpp
+++ Windows/WindowsWoL.cpp
@@ -10,7 +10,7 @@
 using namespace std;
 
 // 分割 Mac 地址
-bool splitMac(char *input, unsigned char macAddress1[6])
+int splitMac(char *input, unsigned char macAddress1[6])
 {
     // 输入的 Mac 地址
     cout << "输入的 Mac 地址：" << input << endl;
@@ -26,8 +26,8 @@ bool splitMac(char *input, unsigned char macAddress1[6])
     }
     else
     {
-        cout << "Mac 地址格式错误" << endl;
-        return false;
+        // Mac 地址格式错误
+        return 1;
     }
     char *p;
     p = strtok(input, delimiters);
@@ -37,7 +37,7 @@ bool splitMac(char *input, unsigned char macAddress1[6])
         sscanf(p, "%hhx", &macAddress1[i]);
         p = strtok(NULL, delimiters);
     }
-    return true;
+    return 0;
 }
 
 // 数据包打包
@@ -59,45 +59,31 @@ void packaging(unsigned char macAddress1[6], char data1[102])
         data1[i + 4] = macAddress1[4];
         data1[i + 5] = macAddress1[5];
     }
+}
 
-    // 输出 Magic Packet
+//
+void printMagicPacket(char data[102])
+{
     printf("Magic Packet：");
     unsigned char udata[102];
     for (int i = 0; i < 102; i++)
     {
-        udata[i] = data1[i];
-        printf("%x ", udata[i]);
+        udata[i] = data[i];
+        printf("%X ", udata[i]);
     }
     printf("\n");
 }
 
-// 发送 Magic Packet
-bool sendMagicPacket(SOCKET sock, char data[102])
-{
-    SOCKADDR_IN addr;
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(9);
-    addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
-
-    int result = sendto(sock, data, sizeof(data), 0, (SOCKADDR *)&addr, sizeof(addr));
-    if (result == SOCKET_ERROR)
-    {
-        cout << "发送数据失败！" << endl;
-        closesocket(sock);
-        WSACleanup();
-        return false;
-    }
-    cout << "魔法包已发送！" << endl;
-    closesocket(sock);
-    WSACleanup();
-    return true;
-}
-
 int main(int argc, char *argv[])
 {
-    char data[102];
-    unsigned char macAddress[6];
+    // data[102] 存储 Magic Packet
+    char data[102] = {0x00};
+    // 字符指针 inputMacAddress，指向输入参数 argv[1]
     char *inputMacAddress;
+    // 无符号的 macAddress[6] 字符数组存储分割好的 Mac 地址
+    unsigned char macAddress[6] = {0x00};
+    // 整型 result，存储参数调用结果
+    int result = 0;
 
     if (argv[1])
     {
@@ -106,23 +92,25 @@ int main(int argc, char *argv[])
     }
     else
     {
-        // 提示输入参数
+        // 没有输入参数 则提示输入 Mac 地址
         std::string strtmp;
         cout << "请输入 Mac 地址（以“:”或“-”分割的格式）：";
         cin >> strtmp;
+        // 将输入数据存储在 strtmp 中，然后让 inputMacAddress 指向 strtmp[0]
         inputMacAddress = &strtmp[0];
     }
-    
+
     // 分割输入的 Mac 地址
-    if (splitMac(inputMacAddress, macAddress) != true)
+    result = splitMac(inputMacAddress, macAddress);
+    if (result == 1)
     {
-        // 中断退出
+        cout << "Mac 地址格式错误" << endl;
         return 1;
     }
 
     // 初始化 WinSock
     WSADATA wsaData;
-    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    result = WSAStartup(MAKEWORD(2, 2), &wsaData);
     if (result != 0)
     {
         cout << "初始化WinSock失败！" << endl;
@@ -149,9 +137,35 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    // 设置目标地址
+    SOCKADDR_IN addr;
+    // 使用IPv4协议
+    addr.sin_family = AF_INET;
+    // 目标端口号为9
+    addr.sin_port = htons(9);
+    // 目标地址为广播地址
+    addr.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
     // 打包 Magic Packet
     packaging(macAddress, data);
+
+    // 输出 Magic Packet
+    printMagicPacket(data);
+
     // 发送数据
-    sendMagicPacket(sock, data);
+    result = sendto(sock, data, sizeof(data), 0, (SOCKADDR *)&addr, sizeof(addr));
+    if (result == SOCKET_ERROR)
+    {
+        cout << "发送数据失败！" << endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    // 发送成功 关闭 Socket，清理 WinSock
+    cout << "魔法包已发送！" << endl;
+    closesocket(sock);
+    WSACleanup();
+
     return 0;
 }
